a291c348def699aaea55ffaf4c18dcc21fd617b1
Okafor Ifeanyi <zeusifeanyi058@gmail.com>
2022-09-30 08:57:24 +0100

    Everything is Learnable

---
+++ README.md
@@ -0,0 +1,3 @@
+My Test Answer for the Learn able internship program
+
+I'm a big fan of you guys work and pray I get in.
---
+++ nth_rarest.py
@@ -0,0 +1,37 @@
+count = dict()
+
+def nth_most_rate_signature(list_nums, n):
+ 
+    # Verifies if the list_nums is actually a list and the n is actually an integer
+    if type(list_nums) is list and type(n) is int: 
+        # Stores the value in a dict using the key : list_num and value : appearance count  
+        for value in list_nums:
+            count[value] = count.get(value, 0) + 1
+        
+        # Sorts the list to start from the rarest order 
+        list_nums = sorted( [ (value, key) for key, value in count.items() ])
+        # print(list_nums)
+
+        # -1 from n, considering list start from 0
+        if n > len(list_nums):
+            print("n is out of range abeg")
+        else:
+            n -= 1 
+            rarest = list_nums[n]
+
+            # answer
+            print(rarest[1])
+    # If verification @line 6 fails print
+    else:
+        print('''   
+    ========== Error Message =============
+        Please crosscheck your values
+            list_nums = list(), n = integer()
+        ''')
+        # print('list_nums = list(), n = integer(). Please crosscheck your values')
+
+# Calling the function and passing values into the function 
+list_nums = [5,4,5,4,5,4,4,5,3,3,3,2,2,1,5]
+nth_most_rate_signature(list_nums, "Today")
+
+
